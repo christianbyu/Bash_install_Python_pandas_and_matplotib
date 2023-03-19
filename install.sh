e0ed77c03bc0fff3e8ff7cc8c7edf6723c462b0e
pc aing <christianbayu@google.com>
2023-03-19 18:49:20 +0700

    upload code

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installasi Pandas
+apt install python3 -y
+
+apt install pip -y
+
+python3 pip install pandas -y
+
+# Installasi Matplotib
+
+apt install python3 -y
+
+apt install pip -y
+
+python3 pip install matplotib -y
